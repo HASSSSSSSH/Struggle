9f48c51e9c08dca15ece362cb1ad150f321b74e5
HASSH <hassssssssh@gmail.com>
2020-06-19 18:17:12 +0800

    LeetCode-lcci Q0203 Solutions

---
+++ src/questions/Y2020M06D18_LCCI_Q0203_S1.java
@@ -0,0 +1,50 @@
+package questions;
+
+/**
+ * https://leetcode-cn.com/problems/delete-middle-node-lcci/
+ * 删除中间节点
+ * 实现一种算法，删除单向链表中间的某个节点（即不是第一个或最后一个节点），假定你只能访问该节点。
+ */
+public class Y2020M06D18_LCCI_Q0203_S1 {
+
+    public static void main(String args[]) {
+        Y2020M06D18_LCCI_Q0203_S1 instance = new Y2020M06D18_LCCI_Q0203_S1();
+        ListNode head = new ListNode(1);
+        ListNode temp = head;
+        for (int i = 2; i <= 5; i++) {
+            temp.next = new ListNode(i);
+            temp = temp.next;
+        }
+        instance.deleteNode(head.next.next);
+        System.out.println(head);
+    }
+
+    public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+
+    private static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+
+        @Override
+        public String toString() {
+            StringBuilder builder = new StringBuilder();
+            ListNode head = this;
+            while (head != null) {
+                builder.append("node-")
+                        .append(head.hashCode())
+                        .append(": ")
+                        .append(head.val)
+                        .append("\n");
+                head = head.next;
+            }
+            return builder.toString();
+        }
+    }
+}
