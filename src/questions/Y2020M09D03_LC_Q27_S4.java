92ea8c196ec5ea550d62ae33076eea86250e5fe9
HASSH <hassssssssh@gmail.com>
2021-04-30 17:49:13 +0800

    Review LeetCode Q27

--- src/questions/Y2020M09D03_LC_Q27_S4.java
+++ src/questions/Y2020M09D03_LC_Q27_S4.java
@@ -21,12 +21,12 @@ import java.util.Arrays;
  * <p>
  * 空间复杂度: O(1)
  * <p>
- * Complexity: High
+ * Different implementation of {@link Y2020M09D03_LC_Q27_S3}
  */
-public class Y2020M09D02_LC_Q27_S1 {
+public class Y2020M09D03_LC_Q27_S4 {
 
     public static void main(String args[]) {
-        Y2020M09D02_LC_Q27_S1 instance = new Y2020M09D02_LC_Q27_S1();
+        Y2020M09D03_LC_Q27_S4 instance = new Y2020M09D03_LC_Q27_S4();
 
         // int[] nums = new int[]{1, 2, 2, 2, 3, 3};
         int[] nums = new int[]{1, 2};
@@ -36,34 +36,18 @@ public class Y2020M09D02_LC_Q27_S1 {
         System.out.println(Arrays.toString(nums));
     }
 
+    // 元素的顺序可以改变
     public int removeElement(int[] nums, int val) {
-        if (nums == null || nums.length < 1) {
-            return 0;
-        }
         int start = 0;
         int end = nums.length - 1;
-        while (start < end) {
-            for (; start < end; start++) {
-                if (nums[start] == val) {
-                    break;
-                }
-            }
-            if (start >= end) {
-                break;
-            }
-            for (; end > start; end--) {
-                if (nums[end] != val) {
-                    break;
-                }
-            }
-            if (end > start) {
+        while (start <= end) {
+            if (val == nums[start]) {
                 nums[start] = nums[end];
                 end--;
-
-                // can not pass while nums = [1, 2], val = 1
-                // start++;
+            } else {
+                start++;
             }
         }
-        return nums[start] == val ? start : start + 1;
+        return start;
     }
 }
