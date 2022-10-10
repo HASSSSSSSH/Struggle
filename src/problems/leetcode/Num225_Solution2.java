f5f058f4fda9d6a945c414168cdd1e9cf7c8c580
HASSH <hassssssssh@gmail.com>
2022-10-11 00:29:07 +0800

    Review LeetCode 225 & 232

--- src/problems/leetcode/Num225_Solution2.java
+++ src/problems/leetcode/Num225_Solution2.java
@@ -1,14 +1,13 @@
-package questions;
+package problems.leetcode;
 
 import java.util.ArrayDeque;
 import java.util.Deque;
 
 /**
- * https://leetcode-cn.com/problems/implement-stack-using-queues/
+ * https://leetcode.cn/problems/implement-stack-using-queues/
  * 用队列实现栈
  * <p>
  * 请你仅使用队列实现一个后入先出 (LIFO) 的栈, 并支持普通队列的全部四种操作 (push, top, pop 和 empty)
- * <p>
  * 实现 MyStack 类:
  * void push(int x) 将元素 x 压入栈顶
  * int pop() 移除并返回栈顶元素
@@ -16,29 +15,29 @@ import java.util.Deque;
  * boolean empty() 如果栈是空的, 返回 true, 否则, 返回 false
  * <p>
  * 注意:
- * 你只能使用队列的基本操作 -- 也就是 push to back, peek/pop from front, size, 和 is empty 这些操作是合法的
- * 你所使用的语言也许不支持队列, 你可以使用 list 或者 deque (双端队列) 来模拟一个队列, 只要是标准的队列操作即可
- * 你可以假设所有操作都是有效的 (例如, 对一个空的栈不会调用 pop 或者 top 操作)
+ * 你只能使用队列的基本操作, 也就是 push to back, peek/pop from front, size, 和 is empty 这些操作是合法的
+ * 你所使用的语言也许不支持队列, 你可以使用 list (列表) 或者 deque (双端队列) 来模拟一个队列, 只要是标准的队列操作即可
  * <p>
- * Tags: {@link questions.tags.Stack}, {@link questions.tags.Design}
+ * 进阶: 你能否仅用一个队列来实现栈
+ * <p>
+ * Tags: {@link questions.tags.Queue}, {@link questions.tags.Stack}, {@link questions.tags.Design}
  * <p>
  * Review: {@link questions.tags.ReviewLevel#E}
  * <p>
  * Solution: {@link questions.tags.Queue}
  * <p>
- * Reference: https://leetcode-cn.com/problems/implement-stack-using-queues/solution/yong-dui-lie-shi-xian-zhan-by-leetcode-solution/
- * <p>
- * 时间复杂度: 入栈操作 O(n), 其余操作都是 O(1)
+ * 时间复杂度: 入栈操作 O(n), 其余操作都是 O(1), 其中 n 是栈内的元素个数
  * 入栈操作需要将队列中的 n 个元素出队, 并入队 n + 1 个元素到队列, 共有 2n + 1 次操作, 每次出队和入队操作的时间复杂度都是 O(1), 因此入栈操作的时间复杂度是 O(n)
  * 出栈操作对应将队列的前端元素出队, 时间复杂度是 O(1)
  * 获得栈顶元素操作对应获得队列的前端元素, 时间复杂度是 O(1)
  * 判断栈是否为空操作只需要判断队列是否为空, 时间复杂度是 O(1)
  * <p>
- * 空间复杂度: O(n), 其中 n 是栈内的元素, 需要使用一个队列存储栈内的元素
+ * 空间复杂度: O(n), 其中 n 是栈内的元素
+ * 需要使用一个队列存储栈内的元素
  */
-public class Y2021M01D01_LC_Q225_S3 {
+public class Num225_Solution2 {
 
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         MyStack obj = new MyStack();
         obj.push(1);
         obj.push(2);
@@ -55,6 +54,9 @@ public class Y2021M01D01_LC_Q225_S3 {
         System.out.println(obj.empty());
     }
 
+    /**
+     * 用一个队列实现栈
+     */
     static class MyStack {
         Deque<Integer> queue = new ArrayDeque<>();
 
@@ -68,10 +70,15 @@ public class Y2021M01D01_LC_Q225_S3 {
          * Push element x onto stack.
          */
         public void push(int x) {
+            // 首先记下当前容器中元素的数量
             int n = queue.size();
-            queue.addLast(x);
+
+            // 将新增元素放入容器的尾部
+            queue.push(x);
+
+            // 将容器的前 n 个元素按先进先出的顺序放到容器的尾部
             for (int i = 0; i < n; i++) {
-                queue.addLast(queue.pollFirst());
+                queue.push(queue.pop());
             }
         }
 
@@ -79,14 +86,14 @@ public class Y2021M01D01_LC_Q225_S3 {
          * Removes the element on top of the stack and returns that element.
          */
         public int pop() {
-            return queue.isEmpty() ? -1 : queue.pollFirst();
+            return queue.isEmpty() ? -1 : queue.pop();
         }
 
         /**
          * Get the top element.
          */
         public int top() {
-            return queue.isEmpty() ? -1 : queue.peekFirst();
+            return queue.isEmpty() ? -1 : queue.peek();
         }
 
         /**
--- src/problems/leetcode/Num232_Solution1.java
+++ src/problems/leetcode/Num232_Solution1.java
@@ -1,16 +1,15 @@
-package questions;
+package problems.leetcode;
 
 import java.util.ArrayDeque;
 import java.util.Deque;
 
 /**
- * https://leetcode-cn.com/problems/implement-queue-using-stacks/
- * https://leetcode-cn.com/problems/yong-liang-ge-zhan-shi-xian-dui-lie-lcof/
+ * https://leetcode.cn/problems/implement-queue-using-stacks/
+ * https://leetcode.cn/problems/yong-liang-ge-zhan-shi-xian-dui-lie-lcof/
  * 用栈实现队列
  * <p>
  * 请你仅使用两个栈实现先入先出队列
  * 队列应当支持一般队列的支持的所有操作 (push, pop, peek, empty)
- * <p>
  * 实现 MyQueue 类:
  * void push(int x) 将元素 x 推到队列的末尾
  * int pop() 从队列的开头移除并返回元素
@@ -18,10 +17,10 @@ import java.util.Deque;
  * boolean empty() 如果队列为空, 返回 true, 否则, 返回 false
  * <p>
  * 说明:
- * 你只能使用标准的栈操作, 也就是只有 push to top, peek/pop from top, size, 和 is empty 操作是合法的
+ * 你只能使用标准的栈操作, 也就是只有 push to top, peek/pop from top, size, 和 is empty 操作是合法的
  * 你所使用的语言也许不支持栈, 你可以使用 list 或者 deque (双端队列) 来模拟一个栈, 只要是标准的栈操作即可
  * <p>
- * Tags: {@link questions.tags.Stack}, {@link questions.tags.Design}
+ * Tags: {@link questions.tags.Stack}, {@link questions.tags.Queue}, {@link questions.tags.Design}
  * <p>
  * Review: {@link questions.tags.ReviewLevel#E}
  * <p>
@@ -33,9 +32,9 @@ import java.util.Deque;
  * 空间复杂度: O(n), 其中 n 是操作总数
  * 对于有 n 次 push 操作的情况, 队列中会有 n 个元素, 故空间复杂度为 O(n)
  */
-public class Y2020M12D30_LC_Q232_S1 {
+public class Num232_Solution1 {
 
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         MyQueue obj = new MyQueue();
         obj.push(1);
         obj.push(2);
@@ -61,9 +60,8 @@ public class Y2020M12D30_LC_Q232_S1 {
      * boolean param_4 = obj.empty();
      */
     static class MyQueue {
-
-        Deque<Integer> stack1 = new ArrayDeque<>();
-        Deque<Integer> stack2 = new ArrayDeque<>();
+        Deque<Integer> inStack = new ArrayDeque<>();
+        Deque<Integer> outStack = new ArrayDeque<>();
 
         /**
          * Initialize your data structure here.
@@ -75,43 +73,38 @@ public class Y2020M12D30_LC_Q232_S1 {
          * Push element x to the back of queue.
          */
         public void push(int x) {
-            if (stack1.isEmpty()) {
-                while (!stack2.isEmpty()) {
-                    stack1.addLast(stack2.pollLast());
-                }
-            }
-            stack1.addLast(x);
+            inStack.push(x);
         }
 
         /**
          * Removes the element from in front of queue and returns that element.
          */
         public int pop() {
-            if (stack2.isEmpty()) {
-                while (!stack1.isEmpty()) {
-                    stack2.addLast(stack1.pollLast());
+            if (outStack.isEmpty()) {
+                while (!inStack.isEmpty()) {
+                    outStack.push(inStack.pop());
                 }
             }
-            return stack2.isEmpty() ? -1 : stack2.pollLast();
+            return outStack.isEmpty() ? -1 : outStack.pop();
         }
 
         /**
          * Get the front element.
          */
         public int peek() {
-            if (stack2.isEmpty()) {
-                while (!stack1.isEmpty()) {
-                    stack2.addLast(stack1.pollLast());
+            if (outStack.isEmpty()) {
+                while (!inStack.isEmpty()) {
+                    outStack.push(inStack.pop());
                 }
             }
-            return stack2.isEmpty() ? -1 : stack2.peekLast();
+            return outStack.isEmpty() ? -1 : outStack.peek();
         }
 
         /**
          * Returns whether the queue is empty.
          */
         public boolean empty() {
-            return stack1.isEmpty() && stack2.isEmpty();
+            return inStack.isEmpty() && outStack.isEmpty();
         }
     }
 }
